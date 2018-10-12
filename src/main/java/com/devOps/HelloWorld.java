25c4d49b78c71565ba2fa29b78e7236ed9422e3d
DevOpsAnalyzer <43768597+DevOpsAnalyzer@users.noreply.github.com>
2018-10-12 09:36:28 -0500

    Removed { for error build

--- src/main/java/com/devOps/HelloWorld.java
+++ src/main/java/com/devOps/HelloWorld.java
@@ -2,9 +2,9 @@ package com.devOps;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
 
-		System.out.println("Hello World !!!")
+		System.out.println("Hello World !!!");
 
 	}
 
